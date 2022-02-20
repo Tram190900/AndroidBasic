e630749f444015b2067cde60e59c5e695686eefb
Tram <mavutotram123@gmail.com>
2022-02-20 10:19:32 +0700

    commit layout_profile and layout_pw_generator

--- Lab1/app/src/main/java/com/example/lab1/layout_Profile.kt
+++ Lab1/app/src/main/java/com/example/lab1/layout_Profile.kt
@@ -1,14 +1,11 @@
 package com.example.lab1
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
-class layout_code : AppCompatActivity() {
+class layout_Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_layout_code)
-
+        setContentView(R.layout.activity_layout_profile)
     }
 }
--- Lab1/app/src/main/java/com/example/lab1/layout_login.kt
+++ Lab1/app/src/main/java/com/example/lab1/layout_login.kt
@@ -3,6 +3,7 @@ package com.example.lab1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class layout_login : AppCompatActivity() {
@@ -13,6 +14,12 @@ class layout_login : AppCompatActivity() {
         var txt_forgetPW = findViewById<TextView>(R.id.txt_forgetPW)
         txt_forgetPW.setOnClickListener({
             val intent = Intent(this, layout_forgetPW::class.java)
+            startActivity(intent)
+        })
+
+        var btn_login = findViewById<Button>(R.id.btn_login)
+        btn_login.setOnClickListener({
+            val intent = Intent(this,layout_Profile::class.java)
             startActivity(intent)
         })
     }
